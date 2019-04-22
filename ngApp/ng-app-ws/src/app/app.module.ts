1509f64270e86ba77ed285fdccd605b5832dad31
Alex Santos <arkanjo.alex@gmail.com>
2019-04-21 23:45:18 -0300

    progress

--- ngApp/ng-app-ws/src/app/app.module.ts
+++ ngApp/ng-app-ws/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
