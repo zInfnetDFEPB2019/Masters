b437595a84523fc1381ecf7d99f6537e7f234438
Alex Santos <arkanjo.alex@gmail.com>
2019-04-24 17:44:52 -0300

    Table Data: DONE

--- ngApp/ng-app-ws/src/app/core/table-data/table-data.component.ts
+++ ngApp/ng-app-ws/src/app/core/table-data/table-data.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 
+
+declare interface TableData {
+  headerRow: string[];
+  dataRows: string[][];
+}
 
 export interface PeriodicElement {
   name: string;
@@ -10,16 +15,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 
@@ -29,11 +34,37 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./table-data.component.scss']
 })
 export class TableDataComponent implements OnInit {
+  public tableData1: TableData;
+  public tableData2: TableData;
+
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
+
   constructor() { }
 
   ngOnInit() {
+    this.tableData1 = {
+      headerRow: ['ID', 'Name', 'Country', 'City', 'Salary'],
+      dataRows: [
+        ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
+        ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
+        ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
+        ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
+        ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
+        ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
+      ]
+    }
+    this.tableData2 = {
+      headerRow: ['ID', 'Name', 'Country', 'City', 'Salary'],
+      dataRows: [
+        ['1', 'Dakota Rice', '$36,738', 'Niger', 'Oud-Turnhout'],
+        ['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
+        ['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux'],
+        ['4', 'Philip Chaney', '$38,735', 'Korea, South', 'Overland Park'],
+        ['5', 'Doris Greene', '$63,542', 'Malawi', 'Feldkirchen in Kärnten',],
+        ['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester']
+      ]
+    };
   }
 
 }
