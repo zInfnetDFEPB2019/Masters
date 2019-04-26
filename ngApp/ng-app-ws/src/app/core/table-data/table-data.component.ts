a37585df031fb8d73bdb89f141c976d85b725e64
Alex Santos <arkanjo.alex@gmail.com>
2019-04-26 16:04:20 -0300

    tentando implementar o table Angular material

--- ngApp/ng-app-ws/src/app/core/table-data/table-data.component.ts
+++ ngApp/ng-app-ws/src/app/core/table-data/table-data.component.ts
@@ -3,68 +3,93 @@ import { MatTableModule } from '@angular/material/table';
 
 
 declare interface TableData {
-  headerRow: string[];
-  dataRows: string[][];
+	headerRow: string[];
+	dataRows: string[][];
 }
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+	name: string;
+	position: number;
+	weight: number;
+	symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+	{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+	{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+	{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+	{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+	{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+	{ position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+	{ position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+	{ position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+	{ position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+	{ position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
-
 @Component({
-  selector: 'app-table-data',
-  templateUrl: './table-data.component.html',
-  styleUrls: ['./table-data.component.scss']
+	selector: 'app-table-data',
+	templateUrl: './table-data.component.html',
+	styleUrls: ['./table-data.component.scss']
 })
 export class TableDataComponent implements OnInit {
-  public tableData1: TableData;
-  public tableData2: TableData;
+	public tableData1: TableData;
+	public tableData2: TableData;
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+	displayedColumns: string[] = ['name', 'weight', 'symbol', 'position'];
+	columnsToDisplay: string[] = this.displayedColumns.slice();
+	dataSource = ELEMENT_DATA;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-    this.tableData1 = {
-      headerRow: ['ID', 'Name', 'Country', 'City', 'Salary'],
-      dataRows: [
-        ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
-        ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
-        ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
-        ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
-        ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
-        ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
-      ]
-    }
-    this.tableData2 = {
-      headerRow: ['ID', 'Name', 'Country', 'City', 'Salary'],
-      dataRows: [
-        ['1', 'Dakota Rice', '$36,738', 'Niger', 'Oud-Turnhout'],
-        ['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
-        ['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux'],
-        ['4', 'Philip Chaney', '$38,735', 'Korea, South', 'Overland Park'],
-        ['5', 'Doris Greene', '$63,542', 'Malawi', 'Feldkirchen in Kärnten',],
-        ['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester']
-      ]
-    };
-  }
+	ngOnInit() {
+		this.tableData1 = {
+			headerRow: ['ID', 'Name', 'Country', 'City', 'Salary'],
+			dataRows: [
+				['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
+				['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
+				['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
+				['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
+				['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
+				['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
+			]
+		}
+		this.tableData2 = {
+			headerRow: ['ID', 'Name', 'Country', 'City', 'Salary'],
+			dataRows: [
+				['1', 'Dakota Rice', '$36,738', 'Niger', 'Oud-Turnhout'],
+				['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
+				['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux'],
+				['4', 'Philip Chaney', '$38,735', 'Korea, South', 'Overland Park'],
+				['5', 'Doris Greene', '$63,542', 'Malawi', 'Feldkirchen in Kärnten',],
+				['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester']
+			]
+		};
+	}
+
+	// BUTTON Methods
+	addColumn() {
+		const randomColumn = Math.floor(Math.random() * this.displayedColumns.length);
+		this.columnsToDisplay.push(this.displayedColumns[randomColumn]);
+	}
+
+	removeColumn() {
+		if (this.columnsToDisplay.length) {
+			this.columnsToDisplay.pop();
+		}
+	}
+
+	shuffle() {
+		let currentIndex = this.columnsToDisplay.length;
+		while (0 !== currentIndex) {
+			let randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// Swap
+			let temp = this.columnsToDisplay[currentIndex];
+			this.columnsToDisplay[currentIndex] = this.columnsToDisplay[randomIndex];
+			this.columnsToDisplay[randomIndex] = temp;
+		}
+	}
 
 }
