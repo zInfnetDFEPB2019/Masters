acb67a12aac94401528e4247b728a73019aae451
Alex <arkanjo.alex@gmail.com>
2019-05-03 17:40:39 -0300

    LAYOUT: add icon Header

--- ngApp/ng-app-ws/src/app/shared/layout/header/header.component.ts
+++ ngApp/ng-app-ws/src/app/shared/layout/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   isNavbarCollapsed = true;
-  title:string = 'Masters  </>  Jira';
+  
 
   constructor() { }
 
