839506adecaad5847dc1ea487b7ef858d7b7bf68
Alex Santos <arkanjo.alex@gmail.com>
2020-03-05 08:27:44 -0300

    User Compare - fix layout da tela que estava fixa e tamanho minimo de largura

--- ngApp/src/app/pages/user-compare/user-compare.component.ts
+++ ngApp/src/app/pages/user-compare/user-compare.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user-compare',
-  templateUrl: './user-compare.component.html',
-  styleUrls: ['./user-compare.component.scss']
+	selector: 'app-user-compare',
+	templateUrl: './user-compare.component.html',
+	styleUrls: ['./user-compare.component.scss']
 })
 export class UserCompareComponent implements OnInit {
 
-  constructor() { }
+	public arrayCards: Array<any> = Array.from(new Array(12), (val, index) => val);
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
