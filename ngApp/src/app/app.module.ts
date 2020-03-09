23ec9476d5677f8cc4a7ff6bbe6963aa9c1b33f4
Alex Santos <arkanjo.alex@gmail.com>
2020-03-09 08:46:39 -0300

    mudando nome do caminho da pasta nova shared em paginas

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -14,22 +14,22 @@ import { AppComponent } from './app.component';
 
 // Angular MAterial
 import {
-	MatButtonModule,
-	MatCheckboxModule,
-	MatIconModule,
-	MatBadgeModule,
-	MatListModule,
-	MatTableModule,
-	MatExpansionModule,
-	MatFormFieldModule,
-	MatInputModule,
-	MatTabsModule,
-	MatMenuModule
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatListModule,
+    MatTableModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTabsModule,
+    MatMenuModule
 } from '@angular/material';
 
 // Pages
-import { HeaderComponent } from './pages/shared-pages/header/header.component';
-import { FooterComponent } from './pages/shared-pages/footer/footer.component';
+import { HeaderComponent } from './pages/shared/header/header.component';
+import { FooterComponent } from './pages/shared/footer/footer.component';
 import { TableDataComponent } from './pages/home/table-data/table-data.component';
 import { RowDetailsComponent } from './pages/home/table-data/row-details/row-details.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -39,38 +39,38 @@ import { ProjectDetailsComponent } from './pages/project-details/project-details
 import { UserCompareComponent } from './pages/user-compare/user-compare.component';
 import { HallOfFameComponent } from './pages/hall-of-fame/hall-of-fame.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
-import { CardUserComponent } from './pages/shared-pages/card-user/card-user.component';
+import { CardUserComponent } from './pages/shared/card-user/card-user.component';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HeaderComponent,
-		FooterComponent,
-		TableDataComponent,
-		RowDetailsComponent,
-		HomeComponent,
-		AccessComponent,
-		UserDetailsComponent,
-		ProjectDetailsComponent,
-		UserCompareComponent,
-		HallOfFameComponent,
-		NotFoundPageComponent,
-		CardUserComponent
-	],
-	imports: [
-		BrowserModule,
-		NgbModule,
-		FormsModule,
-		RouterModule,
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		//Angular Material
-		MatButtonModule, MatCheckboxModule, MatIconModule, MatListModule, MatTableModule, MatBadgeModule, MatExpansionModule,
-		MatFormFieldModule, MatInputModule, MatTabsModule, MatMenuModule,
-		// import HttpClientModule after BrowserModule.
-		HttpClientModule
-	],
-	providers: [],
-	bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        TableDataComponent,
+        RowDetailsComponent,
+        HomeComponent,
+        AccessComponent,
+        UserDetailsComponent,
+        ProjectDetailsComponent,
+        UserCompareComponent,
+        HallOfFameComponent,
+        NotFoundPageComponent,
+        CardUserComponent
+    ],
+    imports: [
+        BrowserModule,
+        NgbModule,
+        FormsModule,
+        RouterModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        //Angular Material
+        MatButtonModule, MatCheckboxModule, MatIconModule, MatListModule, MatTableModule, MatBadgeModule, MatExpansionModule,
+        MatFormFieldModule, MatInputModule, MatTabsModule, MatMenuModule,
+        // import HttpClientModule after BrowserModule.
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
