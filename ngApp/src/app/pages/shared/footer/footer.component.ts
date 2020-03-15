b594262737bd97bc9c727c540ad34ec6537c7fd7
Alex Santos <arkanjo.alex@gmail.com>
2020-03-14 23:52:41 -0300

    FOOTER - Responsivo para mobile - Add bootstrap media Query

--- ngApp/src/app/pages/shared/footer/footer.component.ts
+++ ngApp/src/app/pages/shared/footer/footer.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-layout-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+	selector: 'app-layout-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  today: number = Date.now();
+	today: number = Date.now();
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
