ec60232a77134fe59a7c213ebf0d30c629afa657
Alex Santos <arkanjo.alex@gmail.com>
2020-03-16 00:41:46 -0300

    USer details - foto e dados basicos DONE

--- ngApp/src/app/pages/user-details/user-details.component.ts
+++ ngApp/src/app/pages/user-details/user-details.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+
+const PROFILE_PIC1 = '../../assets/img/faces/kaci-baum-2.jpg';
+
 @Component({
-  selector: 'app-user-details',
-  templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss']
+	selector: 'app-user-details',
+	templateUrl: './user-details.component.html',
+	styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+	public profileImg: string = PROFILE_PIC1;
+	public userName: string = 'Alex Consagrado Fechamento'
+	public companyName: string = 'Minds At Work'
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
