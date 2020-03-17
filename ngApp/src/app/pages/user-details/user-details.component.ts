3156d1ac271d7e9a0d51d56f77e974f2daf09e9c
Alex Santos <arkanjo.alex@gmail.com>
2020-03-16 23:25:24 -0300

    User Details profile info okay

--- ngApp/src/app/pages/user-details/user-details.component.ts
+++ ngApp/src/app/pages/user-details/user-details.component.ts
@@ -1,22 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, AfterViewInit } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 
 const PROFILE_PIC1 = '../../assets/img/faces/kaci-baum-2.jpg';
 
 @Component({
-	selector: 'app-user-details',
+    selector: 'app-user-details',
 	templateUrl: './user-details.component.html',
 	styleUrls: ['./user-details.component.scss']
 })
-export class UserDetailsComponent implements OnInit {
+export class UserDetailsComponent implements OnInit, AfterViewInit {
 
-	public profileImg: string = PROFILE_PIC1;
+    @HostListener("window:resize", [])
+    public onResize() {
+        this.detectScreenSize();
+        console.log('VIew Resize')
+    }
+
+    public isSmallScreen: boolean = false;
+
+    public profileImg: string = PROFILE_PIC1;
 	public userName: string = 'Alex Consagrado Fechamento'
 	public companyName: string = 'Minds At Work'
 
-	constructor() { }
+	constructor(
+        private breakpointObserver: BreakpointObserver
+    ) { }
 
-	ngOnInit() {
-	}
+	ngOnInit() {  }
+
+    ngAfterViewInit() {
+        this.detectScreenSize();
+        console.log('After view init')
+    }
+
+    private detectScreenSize() {
+        let width = window.innerWidth;
+        this.isSmallScreen = (width < 900);
+    }
 
 }
